6d54edb4130457fd2e965e3c4eaf35542e4c0cfa
Radhika-2002 <2019radhika.katiyara@ves.ac.in>
2024-06-14 00:56:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-//your JS code here. If required.
+document.addEventListener("DOMContentLoaded", () => {
+    const buttons = document.querySelectorAll(".btn");
+    const stopButton = document.querySelector(".stop");
+
+    buttons.forEach(button => {
+        button.addEventListener("click", () => {
+            stopAllSounds();
+            const sound = new Audio(`sounds/${button.getAttribute("data-sound")}.mp3`);
+            sound.play();
+            button.setAttribute("data-playing", true);
+        });
+    });
+
+    stopButton.addEventListener("click", () => {
+        stopAllSounds();
+    });
+
+    function stopAllSounds() {
+        const playingButtons = document.querySelectorAll(".btn[data-playing='true']");
+        playingButtons.forEach(button => {
+            button.removeAttribute("data-playing");
+        });
+        const sounds = document.querySelectorAll("audio");
+        sounds.forEach(sound => sound.pause());
+    }
+});
